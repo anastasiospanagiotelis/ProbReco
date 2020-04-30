4853b909d448c6f650c49af38f1b5bb94da34f0f
Anastasios Panagiotelis <anastasios.panagiotelis@monash.edu>
2020-04-30 16:54:38 +1000

    Change cpp functions to take multiple iterates

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,15 +7,15 @@
 using namespace Rcpp;
 
 // energy_i
-Rcpp::List energy_i(Rcpp::NumericMatrix Sin, Rcpp::NumericVector yin, Rcpp::NumericVector xin, Rcpp::NumericVector xsin, Rcpp::NumericVector Gin);
+Rcpp::List energy_i(Rcpp::NumericMatrix Sin, Rcpp::NumericVector yin, Rcpp::NumericMatrix xin, Rcpp::NumericMatrix xsin, Rcpp::NumericVector Gin);
 RcppExport SEXP _ProbReco_energy_i(SEXP SinSEXP, SEXP yinSEXP, SEXP xinSEXP, SEXP xsinSEXP, SEXP GinSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type Sin(SinSEXP);
     Rcpp::traits::input_parameter< Rcpp::NumericVector >::type yin(yinSEXP);
-    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type xin(xinSEXP);
-    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type xsin(xsinSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type xin(xinSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type xsin(xsinSEXP);
     Rcpp::traits::input_parameter< Rcpp::NumericVector >::type Gin(GinSEXP);
     rcpp_result_gen = Rcpp::wrap(energy_i(Sin, yin, xin, xsin, Gin));
     return rcpp_result_gen;
--- src/scores.cpp
+++ src/scores.cpp
@@ -18,27 +18,47 @@ using Eigen::Matrix;
 struct ip {
   const Matrix<double, Dynamic, Dynamic> S_;
   const Matrix<double, Dynamic, 1> y_;
-  const Matrix<double, Dynamic, 1> x_;
-  const Matrix<double, Dynamic, 1> xs_;
+  const Matrix<double, Dynamic, Dynamic> x_;
+  const Matrix<double, Dynamic, Dynamic> xs_;
   ip(const Matrix<double, Dynamic, Dynamic>& S,
      const Matrix<double, Dynamic, 1>& y,
-     const Matrix<double, Dynamic, 1>& x, 
-     const Matrix<double, Dynamic, 1>& xs) : S_(S),y_(y),x_(x),xs_(xs) { }
+     const Matrix<double, Dynamic, Dynamic>& x, 
+     const Matrix<double, Dynamic, Dynamic>& xs) : S_(S),y_(y),x_(x),xs_(xs) { }
   template <typename T>
   T operator()(const Matrix<T, Dynamic, 1>& Gvec) const{
     int n = S_.rows();
     int m = S_.cols();
+    int Q = x_.cols();
     Matrix<T, Dynamic, 1> g1 = Gvec.topRows(m);
     Matrix<T, Dynamic, 1> g2 = Gvec.bottomRows(n*m);
     Matrix<T, Dynamic, Dynamic> G = to_matrix(g2 , m , n);
     Matrix<T, Dynamic, Dynamic> SG = stan::math::multiply(S_,G);
-    Matrix<T, Dynamic, 1> xr = stan::math::multiply(SG,x_);
-    Matrix<T, Dynamic, 1> dif1 = xr - stan::math::multiply(SG,xs_);
+    Matrix<T, Dynamic, Dynamic> xr = stan::math::multiply(SG,x_);
+    Matrix<T, Dynamic, Dynamic> xrs = stan::math::multiply(SG,xs_);
+    
     Matrix<T, Dynamic, 1> d = stan::math::multiply(S_,g1);
-    Matrix<T, Dynamic, 1> dif2 = (y_-d-xr);
-    T term1 = dif1.transpose() * dif1;
-    T term2 = dif2.transpose() * dif2;
-    return ((0.5 * sqrt(term1) ) - sqrt(term2));
+    Matrix<T, Dynamic, 1> yd = y_-d;
+    
+    Matrix<T, Dynamic, 1> dif1;
+    Matrix<T, Dynamic, 1> dif2;
+    
+    T term1 = 0;
+    T term2 = 0;
+    
+    T ip1;
+    T ip2;
+    
+    for (int j=0; j<Q; j++){
+      dif1 = xr.col(j) - xrs.col(j);
+      dif2 = (yd-xr.col(j));
+      ip1 = dif1.transpose() * dif1;
+      ip2 = dif2.transpose() * dif2;
+      term1 += sqrt(ip1);
+      term2 += sqrt(ip2);
+    }
+    
+    
+    return (((0.5 * term1 ) - term2) / Q );
   }
 };
 
@@ -53,13 +73,13 @@ struct ip {
 // [[Rcpp::export(name=".energy_i")]]
 Rcpp::List energy_i(Rcpp::NumericMatrix Sin, 
                 Rcpp::NumericVector yin,
-                Rcpp::NumericVector xin, 
-                Rcpp::NumericVector xsin,
+                Rcpp::NumericMatrix xin, 
+                Rcpp::NumericMatrix xsin,
                 Rcpp::NumericVector Gin){
    Map<Matrix<double,Dynamic,Dynamic>> S(Rcpp::as<Map<Matrix<double,Dynamic,Dynamic>> >(Sin));
    Map<Matrix<double,Dynamic, 1>> y(Rcpp::as<Map<Matrix<double,Dynamic, 1>> >(yin));
-   Map<Matrix<double,Dynamic, 1>> x(Rcpp::as<Map<Matrix<double,Dynamic, 1>> >(xin));
-   Map<Matrix<double,Dynamic, 1>> xs(Rcpp::as<Map<Matrix<double,Dynamic, 1>> >(xsin));
+   Map<Matrix<double,Dynamic, Dynamic>> x(Rcpp::as<Map<Matrix<double,Dynamic, Dynamic>> >(xin));
+   Map<Matrix<double,Dynamic, Dynamic>> xs(Rcpp::as<Map<Matrix<double,Dynamic, Dynamic>> >(xsin));
    Map<Matrix<double,Dynamic, 1>> G(Rcpp::as<Map<Matrix<double,Dynamic, 1>> >(Gin));
    ip f(S,y,x,xs);
    double fx;
